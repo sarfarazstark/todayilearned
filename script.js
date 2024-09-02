06abb8fd2d6d98a3fc4540cbba5637b1dbfa4b70
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2024-09-02 23:17:41 +0530

    Refactor HTML and CSS for the Today I Learned page

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const formToggle = document.querySelector('.form-toggle');
 const form = document.querySelector('.fact-form');
+const fact = document.querySelector('.fact');
+const factList = document.querySelector('.fact-list');
 let bool = false;
+
 formToggle.addEventListener('click', () => {
 	form.classList.toggle('hidden');
 	if (bool) {
@@ -10,4 +13,10 @@ formToggle.addEventListener('click', () => {
 		formToggle.textContent = 'Close';
 		bool = true;
 	}
+});
+
+fact.addEventListener('click', (e) => {
+	const btn = e.target.closest('button');
+	if (!btn) return;
+	console.log('clicked');
 });
